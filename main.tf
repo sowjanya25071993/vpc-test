5f3145ecec5ce17bc7261e4f2f76d46fb52da16d
sowji <jujjurisowjanya@gmail.com>
2024-01-10 20:29:21 +0530

    new

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "roboshop" {
-    source = "../terraform-aws-vpc"
+    source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
