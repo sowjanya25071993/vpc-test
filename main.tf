6a53ce3d1419c034f888776149eedcebeb5dc334
sowji <jujjurisowjanya@gmail.com>
2024-01-10 18:36:47 +0530

    new

--- main.tf
+++ main.tf
@@ -8,4 +8,5 @@ module "roboshop" {
     public_subnets_cidr = var.public_subnets_cidr
     private_subnets_cidr = var.private_subnets_cidr
     database_subnets_cidr = var.database_subnets_cidr
+    is_peering_required = var.is_peering_required
 }
